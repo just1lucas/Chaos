81a2eae6d4abc68f60ca46cdb969bc3c634db360
just1lucas <151664693+just1lucas@users.noreply.github.com>
2023-12-03 16:57:01 +0100

    Bug fix

--- Lorenzgleichungen.py
+++ Lorenzgleichungen.py
@@ -1,6 +1,8 @@
+#Import
 import matplotlib.pyplot as plt
 import numpy as np
 
+#Definition 3 Lorenzgleichungen
 def Lorenzgleichung(xyz,*,s=10,r=28,b=2.667):
     X,Y,Z=xyz
     x_1 = s*(Y-X)
@@ -8,8 +10,7 @@ def Lorenzgleichung(xyz,*,s=10,r=28,b=2.667):
     z_1 = X*Y-b*Z
     return np.array([x_1,y_1,z_1])
 
-
-
+#Parameter/Anfangswerte
 dt = 0.001
 n = 50000 #s
 
@@ -18,6 +19,8 @@ Y_0 = 1
 Z_0_a = 1.5
 Z_0_b = 1.50003
 
+
+#Erstellung Leerer Arrays
 xyz_list_1 = np.empty((n + 1, 3))
 xyz_list_2 = np.empty((n+1,3))
 xyz_list_1[0]=[X_0,Y_0,Z_0_a]
@@ -25,6 +28,7 @@ xyz_list_2[0]=[X_0,Y_0,Z_0_b]
 
 N_list= np.empty((n+1,1))
 
+#numerische Lösung der Lorenzgleichungen
 for i in range(0,n):
     #print(xyz_list [i])
 
@@ -33,18 +37,70 @@ for i in range(0,n):
 
     N_list[i + 1]=i + 1
 
-x_1=xyz_list_1[:,0]
-x_2=xyz_list_2[:,0]
+#Diagramm1___ einfache numerische Lösung der Lorenzgleichungen
+x1=xyz_list_1[:,0]
+y1=xyz_list_1[:,1]
+z1=xyz_list_1[:,2]
 
-x=(x_2-x_1)
-plt.figure()
-#plt.plot(N_list/3600,x_1)
-plt.plot(N_list/3600,x)
+plt.figure(figsize=(12,6))
+
+plt.subplot(311)
+plt.plot(N_list/3600,x1)
 plt.ylabel("X(t)")
-plt.xlabel("t[h]")
+plt.title("numerische Lösung")
+
+plt.subplot(312)
+plt.plot(N_list/3600,y1)
+plt.ylabel("Y(t)")
+
+plt.subplot(313)
+plt.plot(N_list/3600,z1)
+plt.ylabel("Z(t)")
+plt.xlabel("t  [h]")
+
 plt.show()
 
-ax = plt.figure().add_subplot(projection='3d')
+#Diagramm2___ Phasendiagramm
+ax = plt.figure(figsize=(6,6)).add_subplot(projection='3d')
+
+ax.plot(*xyz_list_1.T, lw=0.5)
+ax.set_xlabel("X(t)")
+ax.set_ylabel("Y(t)")
+ax.set_zlabel("Z(t)")
+ax.set_title("Lorenz Attractor")
+plt.show()
+
+
+#Diagramm3___ zweifache numerische Lösung der Lorenzgleichungen
+
+x2=xyz_list_2[:,0]
+y2=xyz_list_2[:,1]
+z2=xyz_list_2[:,2]
+
+plt.figure(figsize=(12,6))
+
+plt.subplot(311)
+plt.plot(N_list/3600,x1)
+plt.plot(N_list/3600,x2)
+plt.ylabel("X(t)")
+plt.title("numerische Lösung")
+
+plt.subplot(312)
+plt.plot(N_list/3600,y1)
+plt.plot(N_list/3600,y2)
+plt.ylabel("Y(t)")
+
+plt.subplot(313)
+plt.plot(N_list/3600,z1)
+plt.plot(N_list/3600,z2)
+plt.ylabel("Z(t)")
+plt.xlabel("t  [h]")
+
+plt.show()
+
+#Diagramm4___ Lorenz Atraktor für zwei Lösungen
+
+ax = plt.figure(figsize=(6,6)).add_subplot(projection='3d')
 
 ax.plot(*xyz_list_1.T, lw=0.5)
 ax.plot(*xyz_list_2.T, lw=0.5)
